5ce0befdc864447b3769bec30f3351b80de96315
Christopher Tackett <hatackett2013@gmail.com>
2020-07-22 12:25:09 -0400

    Initial commit

---
+++ EvenOrOdd.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number please: "))
+finalOutput = number % 2
+if finalOutput == 0:
+    print(number, "is an even number.".format(number))
+else:
+    print(number, "is an odd number.".format(number))
+
+
+
